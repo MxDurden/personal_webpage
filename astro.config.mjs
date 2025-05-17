ffed3ef8615029558232499b75445556dcfe6d37
mrdurden <iq@magueylabs.com>
2025-05-17 07:27:05 -0600

    Restaurando despues del desmadre

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,7 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  integrations: [tailwind()],
+});
